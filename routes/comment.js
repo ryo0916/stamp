51de0d94877262890ae6ddaaad4c0375c08f542a
Ryo Kawakami <rkganma@gmail.com>
2019-04-08 23:27:01 +0900

    コメントのテストページを追加

---
+++ routes/comment.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+let moment = require('moment');
+let connection = require('../mysqlConnection');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let query = 'SELECT *, DATE_FORMAT(created_at, \'%Y年%m月%d日 %k時%i分%s秒\') AS created_at FROM comments';
+  connection.query(query, function(err, rows) {
+    res.render('comment', {
+      title: 'テストページ',
+      commentList: rows
+     });
+     console.log(rows);
+  });
+
+});
+
+router.post('/', function(req, res, next) {
+  let comment = req.body.comment;
+  let createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
+  let query = 'INSERT INTO comments (comment, created_at) VALUES ("' + comment + '", ' + '"' + createdAt + '")';
+  console.log(comment);
+  console.log(connection.query);
+  connection.query(query, function(err, rows) {
+    res.redirect('/comment');
+  });
+});
+
+module.exports = router;
