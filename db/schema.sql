1f15ef3396fe4bcbf3973935a17f213b61e571c8
Ryo Kawakami <rkganma@gmail.com>
2019-05-02 07:22:03 +0900

    DBスキーマ作成

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE users (
+  user_id INT(11) NOT NULL AUTO_INCREMENT,
+  name  VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  created_at DATETIME NOT NULL
+);
+
+CREATE TABLE markers (
+  marker_id INT(11) NOT NULL AUTO_INCREMENT,
+  map_id INT(11) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  comment TEXT,
+  latitude DECIMAL(12,9) NOT NULL,
+  longitude DECIMAL(12,9) NOT NULL,
+  created_at DATETIME NOT NULL
+);
+
+CREATE TABLE maps (
+  map_id INT(11) NOT NULL AUTO_INCREMENT,
+  map_name VARCHAR(255) NOT NULL,
+  user_id INT(11) NOT NULL,
+  latitude DECIMAL(12,9) NOT NULL,
+  longitude DECIMAL(12,9) NOT NULL,
+  created_at DATETIME NOT NULL
+);
+
+CREATE TABLE comments (
+  comment_id INT(11) NOT NULL AUTO_INCREMENT,
+  user_id INT(11),
+  comment TEXT  NOT NULL,
+  created_at DATETIME NOT NULL
+);
