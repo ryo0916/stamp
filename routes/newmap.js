82e802f48a4515d53836e2d254211574759723e6
Ryo Kawakami <rkganma@gmail.com>
2019-05-05 00:39:54 +0900

    地図作成後にホームではなく地図に直接飛ぶように修正

--- routes/newmap.js
+++ routes/newmap.js
@@ -13,8 +13,8 @@ router.get('/', function(req, res, next) {
       logout: 'ログアウト'
     });
   } else {
-    res.render('newmap', {
-      title: '地図作成',
+    res.render('index', {
+      title: 'stamp',
       register: '新規登録',
       login: 'ログイン'
     })
@@ -29,10 +29,12 @@ router.post('/', function(req, res, next) {
   let createdAt = moment().format('YYYY-MM-DD HH:mm:ss');
   let user_id = req.session.user_id;
   let query = 'INSERT INTO maps (map_name, latitude, longitude, created_at, user_id) VALUES ("' + mapName + '", ' + '"' + latitude + '", ' + '"' + longitude + '", ' + '"' + createdAt + '", ' + '"' + user_id + '")';
-  // let getMapidQuery = 'SELECT * FROM maps WHERE created_at = "' + createdAt + '" LIMIT 1';
+  let getMapidQuery = 'SELECT * FROM maps WHERE created_at = "' + createdAt + '" LIMIT 1';
   connection.query(query, function(err, rows){
-    console.log(query);
-    res.redirect('/');
+    connection.query(getMapidQuery, function(err, rows){
+      const map_id = rows[0].map_id;
+      res.redirect('editmap/' + map_id);
+    })
   });
 });
 
