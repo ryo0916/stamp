f7e0d7691c9e96884cd3478e2ca580aecc9485ee
ryo0916 <rkganma@gmail.com>
2024-03-12 08:44:21 +0900

    dockerフロントエンドのみ対応

--- src/mysqlConnection.js
+++ src/mysqlConnection.js
@@ -2,28 +2,31 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 // local用
-/*
+
 const dbConfig = {
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'stamp'
+  host: 'localhost',
+  port: 3306,
+  user: 'test',
+  password: 'test',
+  database: 'test'
 };
-*/
+
 
 // リリース用
+/*
 const dbConfig = {
   host: 'us-cdbr-iron-east-02.cleardb.net',
   user: 'b62372516e3d30',
   password: process.env.DB_CONFIG_PASSWORD,
   database: 'heroku_9d81f307dfe298a'
 }
-
+*/
 let connection = mysql.createConnection(dbConfig);
 
 // これはHerokuのMySQLのためのハックです。
+/*
 setInterval(function() {
   connection.query('SELECT 1');
 }, 5000);
-
+*/
 module.exports = connection;
